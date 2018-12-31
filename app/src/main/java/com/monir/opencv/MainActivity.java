db9bd77499afbfec33eb54bd327ba38f73df781b
z4zzaman <zzaman08@gmail.com>
2019-01-01 00:31:24 +0600

    first commit

---
+++ app/src/main/java/com/monir/opencv/MainActivity.java
@@ -0,0 +1,118 @@
+package com.monir.opencv;
+
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.monir.opencv.utils.Constants;
+import com.monir.opencv.utils.ImagePicker;
+import com.monir.opencv.utils.Utilities;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int PICK_IMAGE_ID = 303;
+
+    private String fileLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        readWritePermission();
+
+        Button startButton = (Button)findViewById(R.id.OpenCV_camera_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cvIntent = new Intent(MainActivity.this, OpenCVCamera.class);
+                startActivity(cvIntent);
+            }
+        });
+
+
+        Button scannerButton = (Button)findViewById(R.id.document_scanner_button);
+        scannerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openItent = ImagePicker.getPickImageIntent(MainActivity.this);
+                startActivityForResult(openItent, PICK_IMAGE_ID);
+            }
+        });
+    }
+
+    private void readWritePermission() {
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if(!report.areAllPermissionsGranted()){
+                            Toast.makeText(MainActivity.this, "You need to grant all permission to use this app features", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+
+                    }
+                })
+                .check();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //return selected image as bitmap
+        switch(requestCode) {
+
+            case PICK_IMAGE_ID:
+                Bitmap bitmap = ImagePicker.getImageFromResult(this, resultCode, data);
+                fileLocation = Constants.SCAN_IMAGE_LOCATION + File.separator + Utilities.generateFilename();
+                try {
+
+                    convertBitmapToImage(bitmap, fileLocation);
+                    Toast.makeText(MainActivity.this, "File successfully stored ", Toast.LENGTH_LONG).show();
+                    Intent openImage = new Intent(MainActivity.this, ImageCropActivity.class);
+                    openImage.putExtra("OPEN_IMAGE", fileLocation);
+                    startActivity(openImage);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+                break;
+        }
+    }
+
+    private void convertBitmapToImage(Bitmap bitmap, String filePath) throws IOException {
+        if(!new File(Constants.SCAN_IMAGE_LOCATION).exists()){
+            new File(Constants.SCAN_IMAGE_LOCATION).mkdir();
+        }
+        File outFile = new File(filePath);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(outFile));
+
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, bufferedOutputStream);
+        bufferedOutputStream.close();
+    }
+}
